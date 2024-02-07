67226e2b4064483267e087edb70a0ef981bd8f5b
aaracan246 <acalberto@iesrafaelalberti.es>
2024-02-07 01:50:17 +0100

    creando clases y métodos

---
+++ src/Pedido.kt
@@ -0,0 +1,24 @@
+class Pedido {
+    companion object{
+        //No pertenece a ningún pedido en concreto, sino que es un valor global a todos y se utilizará para calcular automáticamente los números de los pedidos.
+    }
+
+    fun agregarPlato(plato: Plato){
+
+    }
+
+    fun eliminarPlato(nombrePlato: String){
+
+    }
+
+    fun calcularPrecio(){
+
+    }
+
+    fun calcularTiempo(){
+
+    }
+
+    override fun toString() = ""
+
+}
